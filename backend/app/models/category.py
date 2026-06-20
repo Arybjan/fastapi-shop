947bebd7bf392f6723090dccc77bb77ac5cc47fe
Арыбжан Азизов <arybjen92@gmail.com>
2026-06-20 18:00:17 +0600

    add models for category and products

---
+++ backend/app/models/category.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False, index=True)
+    sub = Column(String, unique=True, nullable=False, index=True)
+
+    products = relationship("Products", back_populates="category")
+
+    def __repr__(self):
+        return f"<Category(id={self.id}, name={self.name})>"
